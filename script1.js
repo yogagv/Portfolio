a0dfa634239f6d9acf1b0a83611defa0e64d858a
yogagv <152758445+yogagv@users.noreply.github.com>
2024-04-29 15:18:38 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,12 @@
+
+function newProduct() {
+    alert("The item You clicked is not available");
+}
+
+const mayura = document.querySelector('.success');
+mayura.addEventListener("onclick", myMayura() );
+
+function myMayura () 
+{
+   alert("The item You clicked is not available")
+}
